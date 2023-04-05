39274aa709395a779b3863a762a3cf7a3c057e25
minhajul-im <minhajul.cpp@gmail.com>
2023-04-05 22:36:16 +0600

    Number to String Convert

--- Common Problems/Primitive Type/string.js
+++ Common Problems/Primitive Type/string.js
@@ -1,10 +1,13 @@
-// 1. reverse string 
-const string = 'this is javascript'
-const reverseStr = (str) => str.split(' ').reverse().join(' ');
+// 1. reverse string
+const string = "this is javascript";
+const reverseStr = (str) => str.split(" ").reverse().join(" ");
 
-// 2. remove vowel in string 
-const vowelStr = 'hello  come to the point';
-const removeVowel =  (str) => str.replace(/[aeiou]/g, '');
+// 2. remove vowel in string
+const vowelStr = "hello  come to the point";
+const removeVowel = (str) => str.replace(/[aeiou]/g, "");
 
 // 4. remove all symbols in string
-const remove = s => s.replace(/[!()'"]/g, '').concat('!');
+const remove = (s) => s.replace(/[!()'"]/g, "").concat("!");
+
+// 5. Number to String
+const numberToString = (num) => num.toString();
