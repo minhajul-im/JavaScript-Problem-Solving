20ed68f39256f4f9345ce08ec711d20f73ea68cd
minhajul-im <minhajul.cpp@gmail.com>
2023-04-08 23:02:37 +0600

    Generate Random Number in Max & Min

--- Common Problems/Number Type/number.js
+++ Common Problems/Number Type/number.js
@@ -1,1 +1,8 @@
+//1. this is called number
 const number = 1971;
+
+// 2 Generate Random Number in Max & Min
+const randomNumber = (max, min) => {
+  const rand = Math.trunc(Math.random() * (max - min) + min);
+  return rand;
+};
