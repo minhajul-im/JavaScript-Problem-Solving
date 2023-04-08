b56aed4057155c128d490915490b884827c7e9a7
minhajul-im <minhajul.cpp@gmail.com>
2023-04-08 23:15:55 +0600

    Multidimensional Array Sumation

--- Common Problems/Number Type/number.js
+++ Common Problems/Number Type/number.js
@@ -6,3 +6,21 @@ const randomNumber = (max, min) => {
   const rand = Math.trunc(Math.random() * (max - min) + min);
   return rand;
 };
+
+// 3 multidimensional arr sum
+const matrix = [
+  [10, 20, 30, 50],
+  [40, 70, 80, 10],
+  [90, 60, 50, 90],
+  [100, 130, 120, 80],
+];
+
+const multiDimensionalArrayOfSum = (matrix) => {
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      sum += matrix[i][j];
+    }
+  }
+  return sum;
+};
