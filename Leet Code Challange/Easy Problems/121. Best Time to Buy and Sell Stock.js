a808570e51643ade74197d1a3b7c503c987dc939
minhajul-im <minhajul.cpp@gmail.com>
2023-05-04 10:37:32 +0600

    121. Best Time to Buy and Sell Stock solved

---
+++ Leet Code Challange/Easy Problems/121. Best Time to Buy and Sell Stock.js
@@ -0,0 +1,19 @@
+var maxProfit = function (prices) {
+  let left = 0; // buy price index
+  let right = 1; // sell price index
+  let maximumProfit = 0; // max profit
+
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      let profit = prices[right] - prices[left];
+
+      maximumProfit = Math.max(maximumProfit, profit);
+    } else left = right;
+
+    right++;
+  }
+
+  return maximumProfit;
+};
+let prices = [7, 1, 5, 3, 6, 4];
+console.log(maxProfit(prices));
