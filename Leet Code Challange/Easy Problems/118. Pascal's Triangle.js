f5e41d18075f4c5ee6339a5794158af80056cbf2
minhajul-im <minhajul.cpp@gmail.com>
2023-05-05 22:37:45 +0600

    118. Pascal's Triangle solved

---
+++ Leet Code Challange/Easy Problems/118. Pascal's Triangle.js
@@ -0,0 +1,18 @@
+var generate = function (numRows) {
+  const result = [];
+  if (numRows >= 1) {
+    result.push([1]);
+  }
+  if (numRows >= 2) {
+    for (let i = 1; i < numRows; i++) {
+      const temp = [1];
+      result[i - 1].forEach((e, i, arr) => {
+        if (i == 0) return;
+        temp.push(e + arr[i - 1]);
+      });
+      temp.push(1);
+      result.push(temp);
+    }
+  }
+  return result;
+};
