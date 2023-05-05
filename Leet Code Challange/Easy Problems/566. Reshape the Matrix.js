82ff24014bbbbaf77068a69106032963a12e1f37
minhajul-im <minhajul.cpp@gmail.com>
2023-05-05 20:10:42 +0600

    566. Reshape the Matrix solved

---
+++ Leet Code Challange/Easy Problems/566. Reshape the Matrix.js
@@ -0,0 +1,24 @@
+let mat = [
+    [1, 2],
+    [3, 4],
+  ],
+  r = 2,
+  c = 4;
+
+console.log(mat.flat());
+
+var matrixReshape = function (mat, r, c) {
+  const flat = mat.flat();
+
+  if (flat.length !== r * c) return mat;
+
+  const reshaped = [];
+  for (let i = 0; i < r; i++) {
+    reshaped.push([]);
+    for (let j = 0; j < c; j++) {
+      reshaped[i][j] = flat.shift();
+    }
+  }
+  return reshaped;
+};
+console.log(matrixReshape(mat));
