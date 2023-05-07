d59776470c5beb774e63df19030193ce0a718c50
minhajul-im <minhajul.cpp@gmail.com>
2023-05-07 12:14:09 +0600

    387. First Unique Character in a String solved Hash-Table

--- Leet Code Challange/Easy Problems/387. First Unique Character in a String.js
+++ Leet Code Challange/Easy Problems/387. First Unique Character in a String.js
@@ -1,4 +1,27 @@
+// hash table
+
 var firstUniqChar = function (s) {
+  let map = new Map();
+
+  for (let char of s) {
+    if (!map.has(char)) {
+      map.set(char, 1);
+    } else {
+      map.set(char, map.get(char) + 1);
+    }
+  }
+
+  for (let char of s) {
+    if (map.get(char) === 1) {
+      return s.indexOf(char);
+    }
+  }
+
+  return -1;
+};
+
+// built-in method.
+var firstUniqCharB = function (s) {
   for (var i = 0; i < s.length; i++) {
     if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
       return i;
@@ -10,4 +33,4 @@ var firstUniqChar = function (s) {
 let s = "leetcode",
   x = "aabb";
 
-console.log(firstUniqChar(x));
+console.log(firstUniqChar(s));
