1c45c5e248e50d957968aa8d7b2f2dbc1b0bd878
minhajul-im <minhajul.cpp@gmail.com>
2023-05-09 11:31:10 +0600

    206. Reverse Linked List 14 days solved recursive solved

--- Leet Code 14 Days Challange/Easy Problems/206. Reverse Linked List.js
+++ Leet Code 14 Days Challange/Easy Problems/206. Reverse Linked List.js
@@ -1,4 +1,6 @@
 // 206. Reverse Linked List
+
+// linked list
 var reverseList = function (head) {
   let pre = null,
     post = null;
@@ -10,4 +12,15 @@ var reverseList = function (head) {
     pre.next = post;
   }
   return pre;
+};
+
+// recursive
+var reverseList = function (head, pre = null) {
+  if (!head) return pre;
+
+  const node = head.next;
+
+  head.next = pre;
+
+  return reverseList(node, head);
 };
