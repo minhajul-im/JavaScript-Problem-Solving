8b10ff38265bf04182fad50953b68274856c570f
minhajul-im <minhajul.cpp@gmail.com>
2023-05-11 14:39:03 +0600

    2629. Function Composition solved

---
+++ JS 30 Days Challenge Leet Code/Easy Problems/2629. Function Composition.js
@@ -0,0 +1,11 @@
+// 2629. Function Composition
+
+var compose = function (functions) {
+  return function (x) {
+    for (let i = functions.length - 1; i >= 0; i--) {
+      x = functions[i](x);
+    }
+
+    return x;
+  };
+};
