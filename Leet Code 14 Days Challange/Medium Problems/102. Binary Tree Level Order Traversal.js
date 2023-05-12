fae6e3e9ed31e65dfe7a70bc3a1677d52e1581f9
minhajul-im <minhajul.cpp@gmail.com>
2023-05-12 11:07:58 +0600

    102. Binary Tree Level Order Traversal solved

---
+++ Leet Code 14 Days Challange/Medium Problems/102. Binary Tree Level Order Traversal.js
@@ -0,0 +1,54 @@
+// 102. Binary Tree Level Order Traversal
+
+var levelOrder = function (root) {
+  if (!root) return [];
+
+  let queue = [root],
+    result = [];
+
+  while (queue.length) {
+    let levelArr = [];
+
+    let length = queue.length;
+
+    for (let i = 0; i < length; i++) {
+      let temp = queue.shift();
+
+      if (temp.left) {
+        queue.push(temp.left);
+      }
+      if (temp.right) {
+        queue.push(temp.right);
+      }
+
+      levelArr.push(temp.val);
+    }
+
+    result.push(levelArr);
+  }
+
+  return result;
+};
+
+/// recursive solution
+var levelOrder = function (root) {
+  let result = [],
+    count = 0;
+
+  return level(root, count, result);
+};
+
+function level(root, count, result) {
+  if (!root) return result;
+
+  if (!result[count]) {
+    result[count] = [root.val];
+  } else {
+    result[count].push(root.val);
+  }
+
+  result = level(root.left, count + 1, result);
+  result = level(root.right, count + 1, result);
+
+  return result;
+}
