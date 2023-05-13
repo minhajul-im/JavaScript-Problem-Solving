485fe1164ca48306cf29a8ed1be7eec40bd4fd4d
minhajul-im <minhajul.cpp@gmail.com>
2023-05-13 14:05:04 +0600

    226. Invert Binary Tree solved

---
+++ Leet Code 14 Days Challange/Easy Problems/226. Invert Binary Tree.js
@@ -0,0 +1,12 @@
+// 226. Invert Binary Tree
+var invertTree = function (root) {
+  if (!root) return null;
+
+  let itIsLeftSide = invertTree(root.left);
+  let itIsRightSide = invertTree(root.right);
+
+  root.left = itIsRightSide;
+  root.right = itIsLeftSide;
+
+  return root;
+};
