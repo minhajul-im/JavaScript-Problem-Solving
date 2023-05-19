bfe8980dbea385f145f824d405edc01d94ec9430
minhajul-im <minhajul.cpp@gmail.com>
2023-05-19 16:42:27 +0600

    1351. Count Negative Numbers in a Sorted Matrix solved

---
+++ Binary Search Challenge/Easy Problems/1351. Count Negative Numbers in a Sorted Matrix.js
@@ -0,0 +1,29 @@
+// 1351. Count Negative Numbers in a Sorted Matrix
+
+let grid = [
+  [4, 3, 2, -1],
+  [3, 2, 1, -1],
+  [1, 1, -1, -2],
+  [-1, -1, -2, -3],
+];
+
+function isNegative(num) {
+  if (Math.sign(num) == -1) {
+    return true;
+  }
+  return false;
+}
+var countNegatives = function (grid) {
+  let nums = grid.flat();
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (isNegative(nums[i])) {
+      count++;
+    }
+  }
+
+  return count;
+};
+
+console.log(countNegatives(grid));
