746a3e9588475085d907b57e93dcd14716ce13cc
minhajul-im <minhajul.cpp@gmail.com>
2023-06-03 11:13:43 +0600

    1299. Replace Elements with Greatest Element on Right Side solved another

--- Leet Code Random/1299. Replace Elements with Greatest Element on Right Side.js
+++ Leet Code Random/1299. Replace Elements with Greatest Element on Right Side.js
@@ -1,1 +1,17 @@
 // 1299. Replace Elements with Greatest Element on Right Side
+
+var replaceElements = function (arr) {
+  let temp,
+    large = -1;
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    temp = arr[i];
+    arr[i] = large;
+
+    if (temp > large) {
+      large = temp;
+    }
+  }
+
+  return arr;
+};
