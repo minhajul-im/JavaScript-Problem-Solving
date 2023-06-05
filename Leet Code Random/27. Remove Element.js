3b3cb89ecbede8794f271e39c9a4718c374ae983
minhajul-im <minhajul.cpp@gmail.com>
2023-06-05 15:01:34 +0600

    27. Remove Element another solved

--- Leet Code Random/27. Remove Element.js
+++ Leet Code Random/27. Remove Element.js
@@ -14,3 +14,17 @@ var removeElement = function (nums, val) {
   }
   return start;
 };
+
+var removeElement = function (nums, val) {
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[count] = nums[i];
+
+      count++;
+    }
+  }
+
+  return count;
+};
