d72950a5978bee06e66525c0e00af0fd32605c95
minhajul-im <minhajul.cpp@gmail.com>
2023-09-23 10:38:19 +0600

    random problem 2 solved

---
+++ Common Problems Challenge/Random Problems/RandomProblem2.js
@@ -0,0 +1,34 @@
+/**
+ * Problem - 2
+ত োমাকে একটা function দে ওয়া হবে called “findAddress()” যা ইনপুট হি সে বে একটা object নি বে । Object এর
+তি নটি property থাকবে ।
+Task:
+1. ত োমাকে ১ম sample output এর format এ output return করতে হবে ।
+2. Bonus: যদি object এর ক োন ো property missing থাকে সে ক্ষে ত্রে সে ই অংশটুকু double underscore
+দি য়ে replace হবে । (২য় output এর format এ )
+Input Output
+{street:10,house:“15A”,society:“Earth
+Perfect”}
+10,15A,Earth Perfect
+{street:10,society:“Earth Perfect”} 10,__,Earth Perfect
+{street:10} 10,_,_
+Function definition:
+function findAddress(obj) {
+}
+ */
+
+const findAddress = (obj) => {
+  // destructure
+  const { street, house, society } = obj;
+
+  if (street && house && society) {
+    return `${street}, ${house}, ${society}!`;
+  } else {
+    // just use only ternary operator
+    return `${street ? street : "__"}, ${house ? house : "__"},
+     ${society ? society : "__"}!`;
+  }
+};
+
+const obj = { street: "", house: "", society: "Earth Perfect" };
+console.log(findAddress(obj));
